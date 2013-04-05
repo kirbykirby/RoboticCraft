f37f2303b8a5590df21336c8b2e6c1e3641c3b0d
hendyzone dengz <hendyzone@163.com>
2013-04-05 18:54:11 +0800

    Fixed Bug(Maybe ╮(￣▽￣")╭)

--- rc/client/utils/SoundManager.java
+++
@@ -1,5 +0,0 @@
-package rc.client.utils;
-
-public class SoundManager {
-
-}
--- rc/common/utils/Localization.java
+++ rc/common/utils/Localization.java
@@ -1,4 +1,3 @@
-
 package rc.common.utils;
 
 import java.io.IOException;
@@ -8,37 +7,39 @@ import java.util.Properties;
 
 import rc.common.RoboticCraft;
 
-
-
 /**
- * Mod localization class.
- * @author dengz
+ * Simple mod localization class.
+ * @author Jimeo Wan
+ * @license Public domain
  */
 public class Localization {
 
 	private static class modInfo {
 
-		final String modName, defaultLanguage;
+		final String	modName, defaultLanguage;
 
 		public modInfo(String modName, String defaultLanguage) {
 			this.modName = modName;
 			this.defaultLanguage = defaultLanguage;
 		}
 	}
-	private static String loadedLanguage = getCurrentLanguage();
-	private static Properties defaultMappings = new Properties();
-	private static Properties mappings = new Properties();
-	private static LinkedList<modInfo> mods = new LinkedList<modInfo>();
+
+	private static String				loadedLanguage	= getCurrentLanguage();
+	private static Properties			defaultMappings	= new Properties();
+	private static Properties			mappings		= new Properties();
+	private static LinkedList<modInfo>	mods			= new LinkedList<modInfo>();
 
 	/**
 	 * Adds localization from a given directory. The files must have the same
 	 * name as the corresponding language file in minecraft and a ".properties"
 	 * file extention e.g "en_US.properties"
-	 *
-	 * @param path The path to the localization files
-	 * @param defaultLanguage The default localization to be used when there is
-	 * no localization for the selected language or if a string is missing (e.g.
-	 * "en_US")
+	 * 
+	 * @param path
+	 *            The path to the localization files
+	 * @param defaultLanguage
+	 *            The default localization to be used when there is no
+	 *            localization for the selected language or if a string is
+	 *            missing (e.g. "en_US")
 	 */
 	public static void addLocalization(String path, String defaultLanguage) {
 		mods.add(new modInfo(path, defaultLanguage));
@@ -47,12 +48,12 @@ public class Localization {
 
 	/**
 	 * Get a string for the given key, in the currently active translation.
-	 *
+	 * 
 	 * @param key
 	 * @return
 	 */
 	public static synchronized String get(String key) {
-		if (!getCurrentLanguage().equals(loadedLanguage)) {
+		if(!getCurrentLanguage().equals(loadedLanguage)) {
 			defaultMappings.clear();
 			mappings.clear();
 			for (modInfo mInfo : mods) {
@@ -69,21 +70,22 @@ public class Localization {
 		Properties modMappings = new Properties();
 
 		try {
-			//Load the default language mappings
-			String lang=path + default_language + ".properties";
+			// Load the default language mappings
+			String lang = path + default_language + ".properties";
 			langStream = Localization.class.getResourceAsStream(lang);
-			 if (langStream==null) {
-				   throw new Exception(lang+ " not found");
-				  }
+			if(langStream == null) {
+				throw new Exception(lang + " not found");
+			}
 
 			modMappings.load(langStream);
 			defaultMappings.putAll(modMappings);
 			langStream.close();
 
-			//Try to load the current language mappings. 
-			//If the file doesn't exist use the default mappings.
-			langStream = Localization.class.getResourceAsStream(path + getCurrentLanguage() + ".properties");
-			if (langStream != null) {
+			// Try to load the current language mappings.
+			// If the file doesn't exist use the default mappings.
+			langStream = Localization.class.getResourceAsStream(path
+					+ getCurrentLanguage() + ".properties");
+			if(langStream != null) {
 				modMappings.clear();
 				modMappings.load(langStream);
 			}
@@ -93,7 +95,7 @@ public class Localization {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (langStream != null)
+				if(langStream != null)
 					langStream.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
@@ -104,6 +106,7 @@ public class Localization {
 	private static String getCurrentLanguage() {
 		return RoboticCraft.proxy.getCurrentLanguage();
 	}
+
 	public static String localize(String key) {
 		return Localization.get(key);
 	}
